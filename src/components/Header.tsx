ebac1b0c3ba9ddb8e491d5b820d7e415d2e2dfdc
Shivam <shivamjhax@gmail.com>
2026-04-18 23:10:38 +0530

    Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header className="rounded-3xl border border-stone-800 bg-stone-900/70 p-8 max-w-lg">
+            <p className="text-2xl font-bold uppercase tracking-[0.2em] text-[#367ec9] text-center">Doc Analyser</p>
+        </header>
+    )
+}
